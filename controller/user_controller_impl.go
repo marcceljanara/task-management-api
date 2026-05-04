28fcfb8982371313153fd038345d3581b1f4c8b7
Marccel Janara <nengahyuichiro34@gmail.com>
2026-05-04 15:54:47 +0700

    implement UserController interface and its implementation for user registration and login

---
+++ controller/user_controller_impl.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"marcceljanara/task-management-api/helper"
+	"marcceljanara/task-management-api/model/web"
+	"marcceljanara/task-management-api/service"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type UserControllerImpl struct {
+	UserService service.UserService
+}
+
+func (controller *UserControllerImpl) Register(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	userCreateRequest := web.UserCreateRequest{}
+	helper.ReadFromRequestBody(request, &userCreateRequest)
+
+	userResponse := controller.UserService.Register(request.Context(), userCreateRequest)
+	webResponse := web.WebResponse{
+		Code: 200,
+		Status: "Berhasil mendaftar akun",
+		Data: userResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller *UserControllerImpl) Login(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	userLoginRequest := web.UserLoginRequest{}
+	helper.ReadFromRequestBody(request, &userLoginRequest)
+
+	userResponse := controller.UserService.Login(request.Context(), userLoginRequest)
+
+	cookie := &http.Cookie{
+		Name: "access_token",
+		Value: userResponse,
+		Path: "/",
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+		Secure: false,
+	}
+
+	http.SetCookie(writer, cookie)
+
+	webResponse := web.WebResponse{
+		Code: 200,
+		Status: "Login berhasil!",
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
--- go.mod
+++ go.mod
@@ -2,14 +2,19 @@ module marcceljanara/task-management-api
 
 go 1.25.4
 
+require github.com/go-playground/validator v9.31.0+incompatible
+
 require (
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.9.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
