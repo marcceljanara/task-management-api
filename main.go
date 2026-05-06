c2e8e59f6c0b3a8a366a68e45446975358c5baab
Marccel Janara <nengahyuichiro34@gmail.com>
2026-05-06 21:16:07 +0700

    add task management routes with JWT validation for task operations

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"marcceljanara/task-management-api/app"
 	"marcceljanara/task-management-api/controller"
+	"marcceljanara/task-management-api/middleware"
 	"marcceljanara/task-management-api/repository"
 	"marcceljanara/task-management-api/service"
 	"net/http"
@@ -28,10 +29,18 @@ func main() {
 	userRepository := repository.NewUserRepository()
 	userService := service.NewUserService(userRepository, jwtService, db, validate)
 	userController := controller.NewUserController(userService)
+	taskRepository := repository.NewTaskRepository()
+	taskService := service.NewTaskService(taskRepository, db, validate)
+	taskController := controller.NewTaskController(taskService)
 
 	router := httprouter.New()
 	router.POST("/api/v1/register", userController.Register)
 	router.POST("/api/v1/login", userController.Login)
+	router.POST("/api/v1/tasks", middleware.ValidateJWT(jwtService, taskController.CreateTask))
+	router.GET("/api/v1/tasks", middleware.ValidateJWT(jwtService, taskController.GetAllTasks))
+	router.GET("/api/v1/task/:taskId", middleware.ValidateJWT(jwtService, taskController.GetTaskById))
+	router.PUT("/api/v1/task/:taskId", middleware.ValidateJWT(jwtService, taskController.UpdateTask))
+	router.DELETE("/api/v1/task/:taskId", middleware.ValidateJWT(jwtService, taskController.DeleteTask))
 
 	server := http.Server{
 		Addr:    "localhost:8080",
